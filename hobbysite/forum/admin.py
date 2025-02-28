978451e5f80500ddc48a604822d0f587a6732faf
Ice Pascual <eizekiel.pascual@student.ateneo.edu>
2025-03-01 02:16:00 +0800

    Add admin panels

--- hobbysite/forum/admin.py
+++ hobbysite/forum/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
+from .models import PostCategory, Post
+
+
 # Register your models here.
+class PostCategoryAdmin(admin.ModelAdmin):
+    model = PostCategory
+
+
+class PostAdmin(admin.ModelAdmin):
+    model = Post
+
+
+admin.site.register(PostCategory, PostCategoryAdmin)
+admin.site.register(Post, PostAdmin)
