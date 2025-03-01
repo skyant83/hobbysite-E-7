869552980bf9f3d1f1add8f38196a3855ef880d6
Ice Pascual <eizekiel.pascual@student.ateneo.edu>
2025-03-01 15:39:46 +0800

    Update urls.py on both project and app level

--- hobbysite/forum/urls.py
+++ hobbysite/forum/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from .views import PostListView, PostDetailView
 
 urlpatterns = [
-    path('forum/threads', PostListView.as_view(), name='post_list'),
-    path('forum/thread/<int:pk>', PostDetailView.as_view(), name='post')
+    path('threads', PostListView.as_view(), name='post_list'),
+    path('thread/<int:pk>', PostDetailView.as_view(), name='post')
 ]
 
 app_name = 'forum'
