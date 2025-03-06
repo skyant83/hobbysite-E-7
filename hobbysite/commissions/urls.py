9b7f5a6c0ca3a160d9f463f2c809106835a2116d
NoahJVV <152837418+NoahJVV@users.noreply.github.com>
2025-03-06 23:34:02 +0800

    App created
    - Base app started
    - URLs ready

---
+++ hobbysite/commissions/urls.py
@@ -0,0 +1,7 @@
+# <appname>/urls.py
+from django.urls import path
+from .views import index
+
+urlpatterns = [
+    path('', index, name='index'),
+]
--- hobbysite/hobbysite/urls.py
+++ hobbysite/hobbysite/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
+    path('commissions/', include('commissions.urls', namespace="commissions")),
     path('admin/', admin.site.urls),
 ]
