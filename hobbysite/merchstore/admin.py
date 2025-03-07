9dbdde98d6431eeb5b1ab2b88d2728ce79cb3e8c
Katoteshiku <andre11obrero@gmail.com>
2025-03-07 21:12:26 +0800

    Admin panel implemented

--- hobbysite/merchstore/admin.py
+++ hobbysite/merchstore/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Product, ProductType
 
-# Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    model = Product
+
+
+class ProductTypeAdmin(admin.ModelAdmin):
+    model = ProductType
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(ProductType, ProductTypeAdmin)
