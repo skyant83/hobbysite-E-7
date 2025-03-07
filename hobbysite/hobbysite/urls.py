913b52336895722ca851bab86d79b609291af543
noahv! <noahjacobv@gmail.com>
2025-03-07 23:00:44 +0800

    Finished templates
    - Still doesn't work due to url error
    - Should look good once that ^ is fixed

--- hobbysite/hobbysite/urls.py
+++ hobbysite/hobbysite/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("commissions/", include("commissions.urls", namespace="commissions")),
+    path('commissions/', include('commissions.urls', namespace='commissions')),
 ]
