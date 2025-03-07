ecb3d6d97ccd1c39a0052396d27a6e8c14547b9a
Katoteshiku <andre11obrero@gmail.com>
2025-03-07 20:56:26 +0800

    Views and html files made

--- hobbysite/merchstore/urls.py
+++ hobbysite/merchstore/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from .views import ProductListView, ProductDetailView
 
 urlpatterns = [
-    
+    path('items', ProductListView.as_view(), name='product-list'),
+    path('item/<int:pk>', ProductDetailView.as_view(), name='item'),
 ]
 
-app_name = 'merchstore'
+app_name = "merchstore"
--- hobbysite/merchstore/views.py
+++ hobbysite/merchstore/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from .models import Product
 
-# Create your views here.
+
+class ProductListView(ListView):
+    model = Product
+    template_name = 'merchstore/products_list.html'
+
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'merchstore/product_detail.html'
