7972a68fe53c29919ade918e276c318876947594
Ice Pascual <eizekiel.pascual@student.ateneo.edu>
2025-04-29 01:43:36 +0800

    Add string representation for Profiles

--- hobbysite/user_management/models.py
+++ hobbysite/user_management/models.py
@@ -7,3 +7,6 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     display_name = models.CharField(max_length=63)
     email_address = models.EmailField()
+
+    def __str__(self):
+        return self.display_name
