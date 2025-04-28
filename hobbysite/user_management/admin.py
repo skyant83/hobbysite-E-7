2889df2bc164b1d2027140c5358b0ac2866cf996
Ice Pascual <eizekiel.pascual@student.ateneo.edu>
2025-04-29 00:55:52 +0800

    Add admin panel

--- hobbysite/user_management/admin.py
+++ hobbysite/user_management/admin.py
@@ -1,3 +1,24 @@
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
 from django.contrib import admin
 
+from .models import Profile
+
+
 # Register your models here.
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+
+
+class UserAdmin(BaseUserAdmin):
+    inlines = [ProfileInline,]
+
+
+class ProfileAdmin(admin.ModelAdmin):
+    model = Profile
+
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+admin.site.register(Profile, ProfileAdmin)
