330a034c29b8e63b106799f8109c1768f9237b71
Ice Pascual <eizekiel.pascual@student.ateneo.edu>
2025-04-29 01:10:20 +0800

    Add accounts urls

--- hobbysite/hobbysite/urls.py
+++ hobbysite/hobbysite/urls.py
@@ -24,5 +24,6 @@ urlpatterns = [
     path('wiki/', include('wiki.urls', namespace="wiki")),
     path('merchstore/', include('merchstore.urls', namespace='merchstore')),
     path('commissions/', include('commissions.urls', namespace='commissions')),
-    path('/', include('user_management.urls', namespace='user_management')),
+    path('', include('user_management.urls', namespace='user_management')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
