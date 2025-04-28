439d8fd2c2f7d45d8fbc3faa49c09c09304f2c76
Ice Pascual <eizekiel.pascual@student.ateneo.edu>
2025-04-28 18:35:16 +0800

    Make urls.py PEP-8 compliant

--- hobbysite/user_management/urls.py
+++ hobbysite/user_management/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from .views import ProfileUpdateView
 
 urlpatterns = [
-    path('profile', ProfileUpdateView.as_view(), name='profile-update-view'),
+    path('profile/<int:pk>',
+         ProfileUpdateView.as_view(),
+         name='profile-update-view'),
 ]
 
 app_name = "user_management"
