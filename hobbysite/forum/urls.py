c54d5e481bc13d0b48f7bbb603e6b406aa50773d
Ice Pascual <eizekiel.pascual@student.ateneo.edu>
2025-05-08 02:14:47 +0800

    Prepare urls for new views

--- hobbysite/forum/urls.py
+++ hobbysite/forum/urls.py
@@ -4,7 +4,9 @@ from .views import ThreadListView, ThreadDetailView
 
 urlpatterns = [
     path('threads', ThreadListView.as_view(), name='thread_list'),
-    path('thread/<int:pk>', ThreadDetailView.as_view(), name='thread')
+    path('thread/<int:pk>', ThreadDetailView.as_view(), name='thread'),
+    path('thread/add', ThreadCreateView.as_view(), name='thread_create'),
+    path('thread/<int:pk>/edit', ThreadUpdateView.as_view(), name='thread_update'),
 ]
 
 app_name = 'forum'
