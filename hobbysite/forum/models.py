daad800b18d04bf225293516b2f4d43496c18e10
Ice Pascual <eizekiel.pascual@student.ateneo.edu>
2025-05-08 02:08:38 +0800

    Update models

--- hobbysite/forum/models.py
+++ hobbysite/forum/models.py
@@ -1,30 +1,37 @@
 from django.db import models
 from django.urls import reverse
 
+from hobbysite.user_management import Profile
+
 
 # Create your models here.
-class PostCategory(models.Model):
+class ThreadCategory(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
 
     class Meta:
         ordering = ['name']
-        verbose_name = 'Post category'
-        verbose_name_plural = 'Post categories'
+        verbose_name = 'Thread category'
+        verbose_name_plural = 'Thread categories'
 
     def __str__(self):
         return self.name
 
 
-class Post(models.Model):
+class Thread(models.Model):
     title = models.CharField(max_length=255)
+    author = models.ForeignKey(
+        Profile,
+        on_delete=models.SET_NULL,
+        null=True)
     category = models.ForeignKey(
-        PostCategory,
+        ThreadCategory,
         on_delete=models.SET_NULL,
         null=True)
     entry = models.TextField()
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
+    image = models.ImageField(upload_to='images/forum/', null=True)
 
     class Meta:
         ordering = ['-date_created']
@@ -36,3 +43,19 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return reverse('forum:post', kwargs={'pk': self.pk})
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(
+        Profile,
+        on_delete=models.SET_NULL,
+        null=True)
+    thread = models.ForeignKey(
+        Thread,
+        on_delete=models.CASCADE)
+    entry = models.TextField()
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['date_created']
