a81b7a33954a02b8cc432d5403e411af80f77eb9
Ice Pascual <eizekiel.pascual@student.ateneo.edu>
2025-05-13 00:49:04 +0800

    Add detail, update, and edit views

--- hobbysite/forum/views.py
+++ hobbysite/forum/views.py
@@ -1,8 +1,9 @@
-from django.views.generic import ListView, DetailView
-# from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from user_management.models import Profile
-from .models import Thread
+from .models import Thread, Comment
+from .forms import CommentForm
 
 
 # Create your views here.
@@ -16,11 +17,47 @@ class ThreadListView(ListView):
             current_profile = Profile.objects.get(user=self.request.user)
             context["my_threads"] = \
                 Thread.objects.filter(author=current_profile)
-            context["other_threads"] = Thread.objects.all() \
-                                             .exclude(author=current_profile)
+            context["other_threads"] = \
+                Thread.objects.all() \
+                              .exclude(author=current_profile)
         return context
 
 
 class ThreadDetailView(DetailView):
     model = Thread
     template_name = 'forum/thread_detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        if self.request.user.is_authenticated:
+            context["similar_threads"] = \
+                Thread.objects.filter(category=self.get_object().category)
+            context["comments"] = \
+                Comment.objects.filter(thread=self.get_object())
+        return context
+
+    def post(self, request, *args, **kwargs):
+        comment = CommentForm(request.POST)
+        if comment.is_valid():
+            comment.save(commit=False)
+            comment.author = Profile.objects.get(user=self.request.user)
+            comment.article = self.get_object()
+            comment.save()
+            return self.get(request, *args, **kwargs)
+        else:
+            self.object = self.get(**kwargs)
+            context = self.get_context_data(**kwargs)
+            context['comment_form'] = comment
+            return self.render_to_response(context)
+
+
+class ThreadCreateView(LoginRequiredMixin, CreateView):
+    model = Thread
+    fields = ['title', 'category', 'entry', 'image']
+    template_name = 'forum/thread_create.html'
+
+
+class ThreadUpdateView(LoginRequiredMixin, UpdateView):
+    model = Thread
+    fields = ['title', 'category', 'entry', 'image']
+    template_name = 'forum/thread_edit.html'
