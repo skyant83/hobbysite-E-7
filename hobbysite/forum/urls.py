761f290d9a41fa02f725f340062742ffea5cdbaa
Ice Pascual <eizekiel.pascual@student.ateneo.edu>
2025-05-13 00:48:42 +0800

    Update urls

--- hobbysite/forum/urls.py
+++ hobbysite/forum/urls.py
@@ -1,12 +1,17 @@
 from django.urls import path
 
-from .views import ThreadListView, ThreadDetailView
+from .views import (ThreadListView,
+                    ThreadDetailView,
+                    ThreadCreateView,
+                    ThreadUpdateView)
 
 urlpatterns = [
     path('threads', ThreadListView.as_view(), name='thread_list'),
-    path('thread/<int:pk>', ThreadDetailView.as_view(), name='thread'),
+    path('thread/<int:pk>', ThreadDetailView.as_view(), name='thread_detail'),
     path('thread/add', ThreadCreateView.as_view(), name='thread_create'),
-    path('thread/<int:pk>/edit', ThreadUpdateView.as_view(), name='thread_update'),
+    path('thread/<int:pk>/edit',
+         ThreadUpdateView.as_view(),
+         name='thread_update'),
 ]
 
 app_name = 'forum'
