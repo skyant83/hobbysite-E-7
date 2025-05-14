4bae20728a5fdb0746441db978cb16d941cbb14f
Ice Pascual <eizekiel.pascual@student.ateneo.edu>
2025-05-14 14:36:27 +0800

    Initialize accounts app

---
+++ hobbysite/accounts/admin.py
@@ -0,0 +1,19 @@
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+from django.contrib import admin
+
+from user_management.models import Profile
+
+
+# Register your models here.
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+
+
+class UserAdmin(BaseUserAdmin):
+    inlines = [ProfileInline,]
+
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
