717158383eb4fb8b764de0fa6ea85b4c30972175
Ice Pascual <eizekiel.pascual@student.ateneo.edu>
2025-05-14 21:03:03 +0800

    Add new models

--- hobbysite/merchstore/models.py
+++ hobbysite/merchstore/models.py
@@ -1,6 +1,22 @@
 from django.db import models
 from django.urls import reverse
 
+from user_management.models import Profile
+
+PRODUCT_STATUS = [
+    'Available',
+    'On sale',
+    'Out of stock',
+]
+
+TRANSACTION_STATUS = [
+    'On cart',
+    'To Pay',
+    'To Ship',
+    'To Receive',
+    'Delivered',
+]
+
 
 class ProductType(models.Model):
     name = models.CharField(max_length=255)
@@ -21,14 +37,34 @@ class Product(models.Model):
         null=True,
         related_name='product_type'
     )
+    owner = models.ForeignKey(
+        Profile,
+        on_delete=models.CASCADE
+    )
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
+    stock = models.IntegerField()
+    status = models.CharField(default='Available', choices=PRODUCT_STATUS)
 
     def __str__(self):
         return self.name
 
     def get_absolute_url(self):
-        return reverse('merchstore:item', kwargs={'pk': self.pk})
+        return reverse('merchstore:product_detail', kwargs={'pk': self.pk})
 
     class Meta:
         ordering = ['name']
+
+
+class Transaction(models.Model):
+    buyer = models.ForeignKey(
+        Profile,
+        on_delete=models.SET_NULL
+    )
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.SET_NULL
+    )
+    amount = models.IntegerField()
+    status = models.CharField(choices=TRANSACTION_STATUS)
+    created_on = models.DateTimeField(auto_now_add=True)
