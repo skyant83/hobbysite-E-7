eb9428cb776b4b9f4e5f6364dc866c791a5c6f88
Ice Pascual <eizekiel.pascual@student.ateneo.edu>
2025-05-15 01:06:26 +0800

    Make admin panels for new models

--- hobbysite/merchstore/admin.py
+++ hobbysite/merchstore/admin.py
@@ -1,14 +1,24 @@
 from django.contrib import admin
-from .models import Product, ProductType
+from .models import Product, ProductType, Transaction
+
+
+class TransactionInline(admin.StackedInline):
+    model = Transaction
 
 
 class ProductAdmin(admin.ModelAdmin):
     model = Product
+    inlines = [TransactionInline]
 
 
 class ProductTypeAdmin(admin.ModelAdmin):
     model = ProductType
 
 
+class TransactionAdmin(admin.ModelAdmin):
+    model = Transaction
+
+
 admin.site.register(Product, ProductAdmin)
 admin.site.register(ProductType, ProductTypeAdmin)
+admin.site.register(Transaction, TransactionAdmin)
