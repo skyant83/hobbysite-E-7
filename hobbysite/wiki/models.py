75007262f2a9928a633ce5aa7b7942cadd16c043
Ken Tabo <ken.tabo@student.ateneo.edu>
2025-05-14 12:40:56 +0800

    images now upload to app-specific folder in media

--- hobbysite/wiki/models.py
+++ hobbysite/wiki/models.py
@@ -21,7 +21,7 @@ class Article(models.Model):
     entry = models.TextField(null=True)
     header_image = models.ImageField(
         null=True,
-        upload_to='images/'
+        upload_to='images/wiki'
     )
     author = models.ForeignKey(
         Profile,
