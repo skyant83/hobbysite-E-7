0e65042c4400b6ac44222dafd607808092cc8a1f
Ice Pascual <eizekiel.pascual@student.ateneo.edu>
2025-05-14 18:19:11 +0800

    Implement user and profile registration

--- hobbysite/user_management/urls.py
+++ hobbysite/user_management/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 
-from .views import ProfileUpdateView
+from .views import ProfileUpdateView, ProfileCreateView
 
 urlpatterns = [
     path('<int:pk>',
          ProfileUpdateView.as_view(),
-         name='profile-update-view'),
+         name='profile_update'),
+    path('registration',
+         ProfileCreateView.as_view(),
+         name='profile_create')
 ]
 
 app_name = "user_management"
--- hobbysite/user_management/views.py
+++ hobbysite/user_management/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
-from django.views.generic.edit import UpdateView
+from django.views.generic.edit import UpdateView, FormView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import redirect
 
 from .models import Profile
+from .forms import RegistrationForm
 
 
 # Create your views here.
@@ -10,3 +11,27 @@ class ProfileUpdateView(LoginRequiredMixin, UpdateView):
     model = Profile
     fields = ['display_name']
     template_name = 'user_management/profile_detail.html'
+
+
+class ProfileCreateView(FormView):
+    template_name = 'user_management/profile_create.html'
+    form_class = RegistrationForm
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'] = RegistrationForm()
+        return context
+
+    def post(self, request, *args, **kwargs):
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            profile = Profile(user=user,
+                              display_name=form.cleaned_data['display_name'],
+                              email_address=user.email)
+            profile.save()
+            return redirect('user_management:profile_update', pk=user.pk)
+        else:
+            context = self.get_context_data(**kwargs)
+            context['form'] = form
+            return self.render_to_response(context)
