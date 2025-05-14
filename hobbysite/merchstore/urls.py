b814032996bec8314ccc037ce7b92a36ebfe06b1
Ice Pascual <eizekiel.pascual@student.ateneo.edu>
2025-05-14 19:03:42 +0800

    Prepare urls for future pages

--- hobbysite/merchstore/urls.py
+++ hobbysite/merchstore/urls.py
@@ -2,8 +2,12 @@ from django.urls import path
 from .views import ProductListView, ProductDetailView
 
 urlpatterns = [
-    path('items', ProductListView.as_view(), name='product-list'),
-    path('item/<int:pk>', ProductDetailView.as_view(), name='item'),
+    path('items', ProductListView.as_view(), name='product_list'),
+    path('item/<int:pk>', ProductDetailView.as_view(), name='product_detail'),
+    # path('item/add', ProductCreateView.as_view(), name='product_create'),
+    # path('item/<int:pk>/edit', ProductUpdateView.as_view(), name='product_update'),
+    # path('cart', CartView.as_view(), name='cart_list'),
+    # path('transactions', TransactionListView.as_view(), name='transactions_list'),
 ]
 
 app_name = "merchstore"
