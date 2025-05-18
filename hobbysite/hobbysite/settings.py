d20bdfcb9ee883f8283c5e28571bd9ef32c0831e
skyant83 <skyant83@gmail.com>
2025-05-19 04:58:56 +0800

    modified:   hobbysite/hobbysite/settings.py

--- hobbysite/hobbysite/settings.py
+++ hobbysite/hobbysite/settings.py
@@ -127,10 +127,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATICFILES_DIRS = [
-    BASE_DIR/'static'
-]
 STATIC_URL = '/static/'
+if DEBUG:
+    STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+    ]
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR,'static')
+
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/accounts/login'
 MEDIA_ROOT = BASE_DIR/'media'
